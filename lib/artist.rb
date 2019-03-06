05cc565f1c674b8fb4e33431b064db3f8770c1d0
Lizzie Chan <lchan217@gmail.com>
2019-03-06 23:05:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,19 +10,5 @@ class Artist
   def self.all 
     @@all 
   end
-   def new_song(name, genre)
-    new_song = Song.new(name, self, genre)
-  end
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-  def genres 
-    Song.all.collect do |song|
-      if song.artist == self
-        song.genre
-      end
-    end
-  end
+   
 end
